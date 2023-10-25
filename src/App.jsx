bf52ac5fd080aaf4e9bddfbf643d333cd1eb1201
marcusmichelin <marcusmichelin@gmail.com>
2023-10-25 16:00:10 -0300

    Primeiro commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NewProject from "./components/NewProject.jsx";
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar/>
+      <NewProject/>
+    </main>
+  );
+}
+
+export default App;
